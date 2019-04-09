b5248544c9ba604245b0c127d8b69e3607d1d7f8
Novus7 <novus@hotmail.com>
2019-04-08 20:15:55 -0400

    Exception Handling
    Program now handles some unique possiblilities and has dialogues to match - needs to be commented still.

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include "ui_mainwindow.h"
 namespace Ui {
 class MainWindow;
 }
@@ -18,10 +18,15 @@ public:
 private:
     Ui::MainWindow *ui;
 private slots:
-    void QuestionFormat();
     void SolvePressed();
     void MinimumKnownCheck();
     void ResetButton();
+    void Solution();
+    int ExceptionHandler();
+
+};
+enum Case{
+    Case_Exception=1, Case_NoException=2, Case_T0=3
 };
 
 #endif // MAINWINDOW_H
